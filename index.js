cdc0ebe3286fe00ec2641403a0f0aa55d3e516fe
Bikal <bikaladhikari5@gmail.com>
2024-04-18 12:00:09 +1000

    EJS file type project completion

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    dayType: "a weekday",
-    advice: "Go to work",
+  const today = new Date();
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if (day === 0 || day === 6) {
+    type = "the weekend";
+    adv = "it's time to have some fun";
+  }
+
+  res.render("solution.ejs", {
+    dayType: type,
+    advice: adv,
   });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
 });
