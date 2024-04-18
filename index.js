c0d39d383d697ead2d625541fcaac0e9766cb4fb
Bikal <bikaladhikari5@gmail.com>
2024-04-18 12:06:53 +1000

    EJS tags

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 import express from "express";
 const app = express();
-
 const port = 3000;
 
 app.get("/", (req, res) => {
-  const today = new Date();
-  const day = today.getDay();
-
-  let type = "a weekday";
-  let adv = "it's time to work hard";
-
-  if (day === 0 || day === 6) {
-    type = "the weekend";
-    adv = "it's time to have some fun";
-  }
-
-  res.render("solution.ejs", {
-    dayType: type,
-    advice: adv,
-  });
+  const data = {
+    title: "EJS Tags",
+    seconds: new Date().getSeconds(),
+    items: ["apple", "banana", "cherry"],
+    htmlContent: "<strong>This is some strong text</strong>",
+  };
+  res.render("index.ejs", data);
 });
 
 app.listen(port, () => {
